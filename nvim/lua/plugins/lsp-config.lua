538020d5f3fc9888706110f152cb3cd14fc11009
GrGLeo <graziani.gothe.leo@gmail.com>
2024-10-07 21:23:40 +0000

    change on lsp-config.lua

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -19,6 +19,12 @@ return {
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+      -- Find references
+      vim.api.nvim_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
+
+      -- Rename symbol
+      vim.api.nvim_set_keymap('n', '<leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
+
     end
   }
 }
